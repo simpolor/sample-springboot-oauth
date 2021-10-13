ffff5eae29a8d2f5677b73dbe7f2be0ab47057a5
sy.park <sy.park@powderroom.co.kr>
2021-10-13 19:27:48 +0900

    .

---
+++ client/src/main/java/io/simpolor/client/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package io.simpolor.client.repository;
+
+import io.simpolor.client.repository.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
